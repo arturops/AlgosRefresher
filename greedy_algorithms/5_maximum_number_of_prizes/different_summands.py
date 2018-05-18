ff0594df9beeea49d1780b47c4591a23fc37303b
Arturo Parrales <arturo.ps14@gmail.com>
2018-05-17 23:32:18 -0400

    Type: Update | Added code for an algorithm that find the maximum number of different numbers that added together give a number n that comes from the command line input to the program

--- greedy_algorithms/5_maximum_number_of_prizes/different_summands.py
+++ greedy_algorithms/5_maximum_number_of_prizes/different_summands.py
@@ -2,8 +2,22 @@
 import sys
 
 def optimal_summands(n):
+    """
+	Finds the maximum number of different numbers that added together give n
+    """
     summands = []
-    #write your code here
+    i = 1
+    while(n != 0):
+    	#print('n-i:{}-{} = {}'.format(n,i,n-i))
+    	if(n-i > i or n-i == 0):
+    		summands.append(i)
+    		n -= i
+    		i += 1
+    	else:
+    		i = n
+    	#print(summands)
+    	#print('new n,i: {},{}'.format(n,i))
+
     return summands
 
 if __name__ == '__main__':
