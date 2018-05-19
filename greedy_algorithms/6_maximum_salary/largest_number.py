a2591f60b511b9a8ccad5c0f08a3068ba80bfe92
Arturo Parrales <arturo.ps14@gmail.com>
2018-05-18 22:33:48 -0400

    Type: Update | Implemented a draft of the largest number composer/finder from combining a given set of number

--- greedy_algorithms/6_maximum_salary/largest_number.py
+++ greedy_algorithms/6_maximum_salary/largest_number.py
@@ -9,9 +9,31 @@ def largest_number(a):
         res += x
     return res
 
+
+def largest_num(digits):
+
+	answer = ""
+
+	while len(digits)>0:
+		maxDigit = '0' #-999999999999999999
+		for digit in digits:
+			if num_comparison(digit,maxDigit): # comparing strings first position
+				maxDigit = digit
+		answer += maxDigit
+		digits.remove(maxDigit)
+	return answer
+
+
+def num_comparison(a,b, base=10):
+
+	if int(a[0]) >= int(b[0]):
+			return True
+	return False
+
 if __name__ == '__main__':
     input = sys.stdin.read()
     data = input.split()
     a = data[1:]
-    print(largest_number(a))
+    print(largest_num(a))
     
+
