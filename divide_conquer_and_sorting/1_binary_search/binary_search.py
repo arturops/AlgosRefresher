85aad8dee56d56cdc4b8db7d62fd36cd1f0dfa90
arturops <arturo.ps14@gmail.com>
2018-05-29 22:33:30 -0400

    Type: Update | Added the code that does binary search in a sorted array for n number

--- divide_conquer_and_sorting/1_binary_search/binary_search.py
+++ divide_conquer_and_sorting/1_binary_search/binary_search.py
@@ -3,7 +3,27 @@ import sys
 
 def binary_search(a, x):
     left, right = 0, len(a)
-    # write your code here
+
+    half = (left + right)//2
+    #print('target: {}'.format(x))
+    #print('input array: {}'.format(a))
+    #print('half: {}'.format(half))
+    #print('--------------------------------------------------')
+
+    if a[half] == x:
+        #print('result: {}'.format(half))
+        #print('*********************************************')
+        return half
+    elif half <= 0:
+        #print('result: -1')
+        #print('*********************************************')
+        return -1
+    elif a[half] < x:
+        b = a[(half+1):(right-1)]
+        b.append(a[-1])
+        return binary_search(b,x)
+    elif a[half] > x:
+        return binary_search(a[left:half],x)
 
 def linear_search(a, x):
     for i in range(len(a)):
@@ -19,4 +39,4 @@ if __name__ == '__main__':
     a = data[1 : n + 1]
     for x in data[n + 2:]:
         # replace with the call to binary_search when implemented
-        print(linear_search(a, x), end = ' ')
+        print(binary_search(a, x), end = ' ')
