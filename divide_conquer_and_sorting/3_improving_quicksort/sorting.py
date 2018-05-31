c0e7089d69cbf677a8d975f986ac73446128a56b
arturops <arturo.ps14@gmail.com>
2018-05-30 22:00:27 -0400

    Type: Update | Started an implementation of randomized quick sort using a 3-way partition. The only missing part is to code the partition code

--- divide_conquer_and_sorting/3_improving_quicksort/sorting.py
+++ divide_conquer_and_sorting/3_improving_quicksort/sorting.py
@@ -2,30 +2,58 @@
 import sys
 import random
 
-def partition3(a, l, r):
-    #write your code here
-    pass
+def partition3(array, left, right):
+    x = array[left]  
+    j = left
+    for idx in range(left + 1, right + 1):
+        if array[idx] < x:
+            
+    #pass
 
-def partition2(a, l, r):
-    x = a[l]
-    j = l;
-    for i in range(l + 1, r + 1):
-        if a[i] <= x:
+def partition2(array, left, right):
+    x = array[left]
+    j = left;
+    for i in range(left + 1, right + 1):
+        if array[i] <= x:
             j += 1
-            a[i], a[j] = a[j], a[i]
-    a[l], a[j] = a[j], a[l]
+            array[i], array[j] = array[j], array[i]
+    array[left], array[j] = array[j], array[left]
     return j
 
 
-def randomized_quick_sort(a, l, r):
-    if l >= r:
+def randomized_quick_sort(array, left, right):
+    """
+    Randomized quick sort dividing array in 2 partitions:
+        + Greater than pivot
+        + Equal to or less than pivot
+    """
+    if left >= right:
         return
-    k = random.randint(l, r)
-    a[l], a[k] = a[k], a[l]
+    pivot = random.randint(left, right)
+    a[left], a[pivot] = a[pivot], a[left]
     #use partition3
-    m = partition2(a, l, r)
-    randomized_quick_sort(a, l, m - 1);
-    randomized_quick_sort(a, m + 1, r);
+    pointer = partition2(array, left, right)
+    randomized_quick_sort(array, left, pointer - 1);
+    randomized_quick_sort(array, pointer + 1, right);
+
+
+def randomized_quick_sort3(array, left, right):
+    """
+    Randomized quick sort dividing array in 3 partitions:
+        + Greater than pivot
+        + Less than pivot
+        + Equal to pivot
+    """
+    if left >= right:
+        return
+    pivot = random.randint(left,right)
+    # swap pivot and left
+    array[left], array[pivot] = array[pivot], array[left]
+    # divide in partitions 
+    pointer1, pointer2 = partition3(array, left, right)
+    #recurse on the partitions
+    randomized_quick_sort3(array, left, pointer1 -1)
+    randomized_quick_sort3(array, pointer2 + 1, right)
 
 
 if __name__ == '__main__':
