5c98fcb73b30ff36046e32a0e26bb5be3b655894
arturops <arturo.ps14@gmail.com>
2018-05-30 21:06:06 -0400

    Type: Update | Coded a O(n) algorithm to find the majority in an unsorted array. Missing to code a divide and conquer approach which will be slower with O(nlog n) running time

--- divide_conquer_and_sorting/2_majority_element/majority_element.py
+++ divide_conquer_and_sorting/2_majority_element/majority_element.py
@@ -2,17 +2,40 @@
 import sys
 
 def get_majority_element(a, left, right):
-    if left == right:
-        return -1
-    if left + 1 == right:
-        return a[left]
+    #if left == right:
+    #    return -1
+    #if left + 1 == right:
+    #    return a[left]
     #write your code here
     return -1
+
+
+def get_majority_element_fast(array):
+    """
+    Returns the majority element if there is any, else -1
+    """
+    counts = {}
+    for element in array:
+        if element in counts:
+            counts[element] += 1
+        else:
+            counts[element] = 1
+
+    #print('counts: {}'.format(counts))
+
+    n = len(array)
+    for key,value in counts.items():
+        if value > n//2:
+            return counts[key]
+
+    return -1 
+
 
 if __name__ == '__main__':
     input = sys.stdin.read()
     n, *a = list(map(int, input.split()))
-    if get_majority_element(a, 0, n) != -1:
+    #if get_majority_element(a, 0, n) != -1:
+    if get_majority_element_fast(a) != -1:
         print(1)
     else:
         print(0)
