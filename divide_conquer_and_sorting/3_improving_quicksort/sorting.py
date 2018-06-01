079b07b6f1cea8a6eb4b4975af35f0b126d124f5
arturops <arturo.ps14@gmail.com>
2018-05-31 23:00:42 -0400

    Type: Update | Coded the 3-way partition function for quicksort3 using the Dutch National Flag Algorithm

--- divide_conquer_and_sorting/3_improving_quicksort/sorting.py
+++ divide_conquer_and_sorting/3_improving_quicksort/sorting.py
@@ -3,11 +3,42 @@ import sys
 import random
 
 def partition3(array, left, right):
-    x = array[left]  
-    j = left
-    for idx in range(left + 1, right + 1):
-        if array[idx] < x:
-            
+    """
+    Partitions array in three sections, return two indexes:
+        - end of most left section
+        - start of most right section
+    Uses the Dutch National Flag Algorithm
+    """
+    # handle 2 elements
+    if right - left <= 1:
+        if(array[right] < array[left]):
+            array[left], array[right] = array[right], array[left]
+        return left, right
+
+    # 3 or more elemnts
+    aux = left  # usually 0
+    pivot = array[right]
+    
+    while aux <= right: # quicksort is handling when aux go out of bounds
+
+        if array[aux] < pivot:
+            array[left], array[aux] = array[aux], array[left]
+            left += 1
+            aux += 1
+
+        elif array[aux] == pivot:
+            aux += 1
+
+        elif array[aux] > pivot:
+            array[aux], array[right] = array[right], array[aux]
+            right -= 1
+
+    if left-1 < 0:
+        left = 0
+
+    #print('array: {}\np1: {}\np2: {}'.format(array,left,aux))
+    return  left, aux # pointer to end of low partition, and pointer to start of high partition
+
     #pass
 
 def partition2(array, left, right):
@@ -53,12 +84,12 @@ def randomized_quick_sort3(array, left, right):
     pointer1, pointer2 = partition3(array, left, right)
     #recurse on the partitions
     randomized_quick_sort3(array, left, pointer1 -1)
-    randomized_quick_sort3(array, pointer2 + 1, right)
+    randomized_quick_sort3(array, pointer2, right)
 
 
 if __name__ == '__main__':
     input = sys.stdin.read()
     n, *a = list(map(int, input.split()))
-    randomized_quick_sort(a, 0, n - 1)
+    randomized_quick_sort3(a, 0, n - 1)
     for x in a:
         print(x, end=' ')
