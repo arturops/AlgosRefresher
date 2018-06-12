5b1bebdbebffd07e25fff4b1661a9c44bb64d87e
Arturo Parrales <arturo.ps14@gmail.com>
2018-06-12 17:06:51 -0400

    Type: Update | Created a fucntion that computes the bottom-up matrix of dynamic programming for a change exchanger application. It finds the minimum number of coins to return for a given amount of money.

--- dynamic_programming_part1/1_money_change_again/change_dp.py
+++ dynamic_programming_part1/1_money_change_again/change_dp.py
@@ -1,9 +1,34 @@
 # Uses python3
 import sys
 
-def get_change(m):
-    #write your code here
-    return m // 4
+#Program to exchange money into the minimum amount of coins
+# It uses coin denominations of 1, 3 and 4
+# Use case of dynamic programming bottom-up case
+
+def get_change(m, coin_denominations=[1,3,4]):
+    """
+	m : Money to exchange (limitation: this value must be an integer)
+	Function returns the minimum number of coins (using coin denominations) 
+    """
+    # base case	
+    if m <= 0:
+    	return 0
+
+    # store the number of minimum coins for m amount of money	
+    change_table = {}
+    change_table[0] = 0
+
+    # build matrix and find change on the process
+    for amount in range(1,m+1):
+    	change_table[amount] = 999999999 #init min change to a big value
+    	for c in coin_denominations:
+    		# ensure amount is greater than any coin denomination (no negatives)
+    		if amount >= c:
+    			min_coins_change = change_table[amount - c] + 1
+    			if min_coins_change < change_table[amount]:
+    				change_table[amount] = min_coins_change
+
+    return change_table[m]
 
 if __name__ == '__main__':
     m = int(sys.stdin.read())
